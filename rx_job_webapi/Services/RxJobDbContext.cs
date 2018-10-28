5b383b4fba933393a6134f0b9eef564ba2916d1d
Luis Del Valle <luisdvalle@users.noreply.github.com>
2018-10-28 23:09:38 +1100

    Fixing DbSets names and Server for connection string

--- rx_job_webapi/Services/RxJobDbContext.cs
+++ rx_job_webapi/Services/RxJobDbContext.cs
@@ -5,12 +5,12 @@ namespace rx_job_webapi.Services
 {
     public class RxJobDbContext : DbContext
     {
-        public DbSet<RX_Job> RxJobTbl { get; set; }
-        public DbSet<RX_RoomType> RxRoomTypeTbl { get; set; }
+        public DbSet<RX_Job> RX_Job { get; set; }
+        public DbSet<RX_RoomType> RX_RoomType { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseSqlServer(@"Server=(localdb)\SQLEXPRESS; Database=Demo; Trusted_Connection=True");
+            options.UseSqlServer(@"Server=localhost\SQLEXPRESS; Database=Demo; Trusted_Connection=True");
         }
     }
 }
